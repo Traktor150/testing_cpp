75350667429a7266a2cc631808a94f2494d84fd3
William Becker <williambecker321@gmail.com>
2023-07-05 16:00:04 +0200

    uppdates

--- print_se.c
+++ print_se.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 // make a funkton to take an argument of size 1 byte and print it
@@ -11,49 +10,46 @@ void print_complex(char *c) {
     while (*c != '\0') {
         // check if the character is the start of a 2 byte utf-8 character
         if ((*c & 0b11100000) == 0b11000000) {
+            // check if the next byte is not a null character if it is brake
+            if (*(c + 1) == '\0') {
+                break;
+            }
             // creata a switch that checks if the character is å,ä,ö or Å,Ä,Ö
             switch (*c) {
                 case 0b11000010:
                     if (*(c + 1) == 0b10100010) {
                         printf("å\n");
                         c += 2;
-                        continue;
                     }
                     break;
                 case 0b11000011:
                     if (*(c + 1) == 0b10100010) {
                         printf("ä\n");
                         c += 2;
-                        continue;
                     }
                     if (*(c + 1) == 0b10100011) {
                         printf("ö\n");
                         c += 2;
-                        continue;
                     }
                     break;
                 case 0b11000001:
                     if (*(c + 1) == 0b10100010) {
                         printf("Å\n");
                         c += 2;
-                        continue;
                     }
                     if (*(c + 1) == 0b10100011) {
                         printf("Ä\n");
                         c += 2;
-                        continue;
                     }
                     if (*(c + 1) == 0b10100101) {
                         printf("Ö\n");
                         c += 2;
-                        continue;
                     }
                     break;
                     // if it is not a special character print the first byte and continue
                 default:
                     print_simple(*c);
                     c++;
-                    continue;
             }
             continue;
         }
@@ -65,7 +61,9 @@ void print_complex(char *c) {
 }
 
 int main() {
-    char c = 'A';
-    print_simple(c);
+    // print a string using print_complex that contains a,b,c, å,ä,ö and Å,Ä,Ö
+    char str[] = "abcåäöÅÄÖ";
+    printf("%s\n", str);
+    print_complex(str);
     return 0;
 }
